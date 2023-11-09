2f3a6bdb8597cae58d2581e34edd22ae3b4ebdc8
cristian.matzov <cristian.matzov@optimal-industries.com>
2023-11-09 11:19:39 +0200

    .

--- scripts/opt-test-db-restore.sh
+++ scripts/opt-test-db-restore.sh
@@ -5,13 +5,13 @@ ssh optimal@192.168.66.11 "bash -s" << EOF
 
 containers=\$(lxc list --format csv --columns=n | grep opt-tst)
 
-sudo rm -rf /tmp/backups/*
+sudo rm -rf /automations/backups/*
 
 ikea_backup=\$(ls -1t /var/backups/ikea/*.tar | head -n 1)
 verila_backup=\$(ls -1t /var/backups/verila/*.tar | head -n 1)
 
-sudo cp \$ikea_backup /tmp/backups/
-sudo cp \$verila_backup /tmp/backups/
+sudo cp \$ikea_backup /automations/backups/
+sudo cp \$verila_backup /automations/backups/
 
 for container in \$containers; do
     lxc start \$container
